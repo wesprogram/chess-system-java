d4e6fc94c3c65f59377f57efda74dfdbcbc12408
Wesley Souza <wesleyribeiro@alumni.usp.br>
2022-07-11 14:38:40 -0300

    Possible moves of a piece

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -44,6 +44,10 @@ public class ChessMatch {
 	private void validadeSourcePosition(Position position) {
 		if (!board.thereIsAPiece(position)) {
 			throw new ChessException("There is no piece on source position");
+			
+		}
+		if(!board.piece(position).isThereAnyPossibleMove()) {
+			throw new ChessException("There is no possible moves dor the chosen piece");
 		}
 	}
 
--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -15,6 +15,12 @@ public class Rook extends ChessPiece {
 		return "R";
 	}
 	
+	@Override
+	public boolean[][] possibleMoves() {
+		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		return mat;
+	}
+	
 	
 
 }
